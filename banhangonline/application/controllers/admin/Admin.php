e274ad749820e3850523c3cceff8970cdc1ad99f
vudinhquang <vudinhquangk53@gmail.com>
2019-05-12 20:06:27 +0900

    Hien thi danh sach admin len trang admin/index

--- banhangonline/application/controllers/admin/Admin.php
+++ banhangonline/application/controllers/admin/Admin.php
@@ -8,60 +8,75 @@ Class Admin extends MY_Controller{
 		$this->load->model('admin_model');
 	}
 
-	public function create(){
-		$this->data = array(
-			'username' => 'admin1',
-			'password' => 'admin1',
-			'name' => 'Hoc PHP'
-		);
+	// public function create(){
+	// 	$this->data = array(
+	// 		'username' => 'admin1',
+	// 		'password' => 'admin1',
+	// 		'name' => 'Hoc PHP'
+	// 	);
 
-		if($this->admin_model->create($this->data)){
-			echo "Them thanh cong";
-		}else{
-			echo "Khong them thanh cong";
-		}
-	}
+	// 	if($this->admin_model->create($this->data)){
+	// 		echo "Them thanh cong";
+	// 	}else{
+	// 		echo "Khong them thanh cong";
+	// 	}
+	// }
 
-	public function update(){
-		$id = 8;
-		$this->data = array(
-			'username' => 'admin2',
-			'password' => 'admin2',
-			'name' => 'Hoc PHP 2'
-		);
-		if ($this->admin_model->update($id, $this->data)) {
-			echo "Cap nhat thanh cong";
-		}else{
-			echo "Cap nhat khong thanh cong";
-		}
-	}
+	// public function update(){
+	// 	$id = 8;
+	// 	$this->data = array(
+	// 		'username' => 'admin2',
+	// 		'password' => 'admin2',
+	// 		'name' => 'Hoc PHP 2'
+	// 	);
+	// 	if ($this->admin_model->update($id, $this->data)) {
+	// 		echo "Cap nhat thanh cong";
+	// 	}else{
+	// 		echo "Cap nhat khong thanh cong";
+	// 	}
+	// }
 
-	public function delete(){
-		$id = 8;
-		if($this->admin_model->delete($id)){
-			echo "Xoa thanh cong";
-		}else{
-			echo "Xoa khong thanh cong";
-		}
-	}
+	// public function delete(){
+	// 	$id = 8;
+	// 	if($this->admin_model->delete($id)){
+	// 		echo "Xoa thanh cong";
+	// 	}else{
+	// 		echo "Xoa khong thanh cong";
+	// 	}
+	// }
 
-	public function get_info(){
-		$id = 1;
-		$info = $this->admin_model->get_info($id, 'username, password');
-		echo "<pre>";
-		print_r($info);
-	}
+	// public function get_info(){
+	// 	$id = 1;
+	// 	$info = $this->admin_model->get_info($id, 'username, password');
+	// 	echo "<pre>";
+	// 	print_r($info);
+	// }
 
-	public function get_list(){
-		$input = array(
+	// public function get_list(){
+	// 	$input = array(
 			// 'where' => array('id' => 1),
 			// 'limit' => array(1, 0),
 			// 'order' => array('id', 'desc')
 			// 'order' => array('username', 'asc')
-			'like' => array('name', 'mod')
-		);
+	// 		'like' => array('name', 'mod')
+	// 	);
+	// 	$list = $this->admin_model->get_list($input);
+	// 	echo "<pre>";
+	// 	print_r($list);
+	// }
+
+	//--------------------------------------------------------------
+	/*
+	*	Lay danh sach admin
+	*/
+	public function index(){
+		$input = array();
 		$list = $this->admin_model->get_list($input);
-		echo "<pre>";
-		print_r($list);
+		$total = $this->admin_model->get_total();
+
+		$this->data['list'] = $list;
+		$this->data['total'] = $total;
+		$this->data['temp'] = 'admin/admin/index';
+		$this->load->view('admin/main', $this->data);
 	}
 }
--- banhangonline/application/helpers/common_helper.php
+++ banhangonline/application/helpers/common_helper.php
@@ -2,3 +2,11 @@
 function public_url($url = ''){
 	return base_url('public/'.$url);
 }
+
+function pre($data, $exit = true){
+	echo "<pre>";
+	print_r($data);
+	if ($exit) {
+		die();
+	}
+}
